716e27af5c134dabbfb1fbdd682e7ad91fa0b29f
carrington-115 <frumarkcarrington@gmail.com>
2025-01-16 14:09:41 +0530

    completed the onboarding ui screens now switching the registration workflow

---
+++ app/(register)/_layout.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Stack } from "expo-router";
+
+export default function componentName() {
+  return (
+    <>
+      <Stack
+        screenOptions={{
+          headerShown: false,
+        }}
+      >
+        <Stack.Screen name="index" />
+        <Stack.Screen name="register-with-phone" />
+      </Stack>
+    </>
+  );
+}
