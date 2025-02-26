07f6f00c699f30f3fa7ece7e5bdee5f0e834d8e8
carrington-115 <frumarkcarrington@gmail.com>
2025-02-27 04:05:30 +0530

    set up the agent tabs pages

--- app/(agent)/_layout.tsx
+++ app/(agent)/_layout.tsx
@@ -1,6 +1,157 @@
-import { Stack } from "expo-router";
+import { textFontStyles } from "@/constants/fonts";
+import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons";
+import MaterialIcons from "@expo/vector-icons/MaterialIcons";
+import { Tabs } from "expo-router";
 import React from "react";
+import { Text } from "react-native";
 
 export default function componentName() {
-  return <Stack>{/* working on route render */}</Stack>;
+  return (
+    <Tabs>
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Home",
+          tabBarLabel: ({ focused }) => (
+            <>
+              <Text
+                style={
+                  focused
+                    ? { ...textFontStyles.bodySmallBold }
+                    : { ...textFontStyles.bodySmallRegular }
+                }
+              >
+                Home
+              </Text>
+            </>
+          ),
+          tabBarIcon: ({ color, focused }) => (
+            <>
+              {focused ? (
+                <MaterialIcons
+                  name={focused ? "home-filled" : "home"}
+                  size={24}
+                  color={color}
+                />
+              ) : (
+                <MaterialCommunityIcons
+                  name="home-variant-outline"
+                  size={24}
+                  color={color}
+                />
+              )}
+            </>
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Home",
+          tabBarLabel: ({ focused }) => (
+            <>
+              <Text
+                style={
+                  focused
+                    ? { ...textFontStyles.bodySmallBold }
+                    : { ...textFontStyles.bodySmallRegular }
+                }
+              >
+                Home
+              </Text>
+            </>
+          ),
+          tabBarIcon: ({ color, focused }) => (
+            <>
+              {focused ? (
+                <MaterialIcons
+                  name={focused ? "home-filled" : "home"}
+                  size={24}
+                  color={color}
+                />
+              ) : (
+                <MaterialCommunityIcons
+                  name="home-variant-outline"
+                  size={24}
+                  color={color}
+                />
+              )}
+            </>
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Home",
+          tabBarLabel: ({ focused }) => (
+            <>
+              <Text
+                style={
+                  focused
+                    ? { ...textFontStyles.bodySmallBold }
+                    : { ...textFontStyles.bodySmallRegular }
+                }
+              >
+                Home
+              </Text>
+            </>
+          ),
+          tabBarIcon: ({ color, focused }) => (
+            <>
+              {focused ? (
+                <MaterialIcons
+                  name={focused ? "home-filled" : "home"}
+                  size={24}
+                  color={color}
+                />
+              ) : (
+                <MaterialCommunityIcons
+                  name="home-variant-outline"
+                  size={24}
+                  color={color}
+                />
+              )}
+            </>
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Home",
+          tabBarLabel: ({ focused }) => (
+            <>
+              <Text
+                style={
+                  focused
+                    ? { ...textFontStyles.bodySmallBold }
+                    : { ...textFontStyles.bodySmallRegular }
+                }
+              >
+                Home
+              </Text>
+            </>
+          ),
+          tabBarIcon: ({ color, focused }) => (
+            <>
+              {focused ? (
+                <MaterialIcons
+                  name={focused ? "home-filled" : "home"}
+                  size={24}
+                  color={color}
+                />
+              ) : (
+                <MaterialCommunityIcons
+                  name="home-variant-outline"
+                  size={24}
+                  color={color}
+                />
+              )}
+            </>
+          ),
+        }}
+      />
+    </Tabs>
+  );
 }
