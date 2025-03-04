30a273bf556aa9264db35d0f51e382be406ebd27
carrington-115 <frumarkcarrington@gmail.com>
2025-03-05 04:04:05 +0530

    built the chart action element

--- components/organisms/EarningChart.tsx
+++ components/organisms/EarningChart.tsx
@@ -1,24 +1,74 @@
 import appColors from "@/constants/colors";
 import { BarChartElementProps } from "@/constants/types";
 import React from "react";
-import { View, StyleSheet } from "react-native";
+import { View, StyleSheet, TouchableOpacity, Text } from "react-native";
+import BarChartElement from "../atoms/BarChartElement";
+import MaterialIcons from "@expo/vector-icons/MaterialIcons";
+import { textFontStyles } from "@/constants/fonts";
 
 export default function componentName({
   bars,
+  date,
+  leftAction,
+  rightAction,
 }: {
   bars: BarChartElementProps[];
+  date: {
+    month: string;
+    initialDate: number;
+    finalDate: number;
+  };
+  leftAction: () => void;
+  rightAction: () => void;
 }) {
   return (
     <View style={styles.container}>
-      <View></View>
+      <View
+        style={{
+          flexDirection: "row",
+          justifyContent: "space-around",
+          alignItems: "center",
+        }}
+      >
+        <ChartActionBtn icon={"arrow-back-ios"} action={leftAction} />
+        <>
+          <Text
+            style={{
+              ...textFontStyles.titleMediumMedium,
+              color: appColors.onSurface,
+            }}
+          >
+            {date.month} {date.initialDate} - {date.finalDate}
+          </Text>
+        </>
+        <ChartActionBtn icon={"arrow-forward-ios"} action={rightAction} />
+      </View>
       <View
         style={{
           width: "100%",
         }}
-      ></View>
+      >
+        {bars.map((bar, index) => (
+          <BarChartElement key={index} {...bar} />
+        ))}
+      </View>
     </View>
   );
 }
+
+const ChartActionBtn = ({
+  icon,
+  action,
+}: {
+  icon: any;
+  action: () => void;
+}) => {
+  return (
+    <TouchableOpacity onPress={action}>
+      <MaterialIcons name={icon} size={24} color={appColors.onSurface} />
+    </TouchableOpacity>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
