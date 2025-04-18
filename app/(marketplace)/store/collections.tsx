9c2fc4546547bd4d92e8abcad8510952e6201da6
carrington-115 <frumarkcarrington@gmail.com>
2025-04-18 10:58:56 +0530

    fixed the content on the collections page

--- app/(marketplace)/store/collections.tsx
+++ app/(marketplace)/store/collections.tsx
@@ -1,15 +1,57 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { View, Text, StyleSheet, Dimensions, ScrollView } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import appColors from "@/constants/colors";
 import { Appbar } from "react-native-paper";
 import { useRouter } from "expo-router";
 import { CollectionsComponent } from "@/components";
+import { NoElementOnPage } from "@/components/organisms/NoElementOnPage";
+import { CollectionsComponentProps } from "@/constants/types";
 
-const { width } = Dimensions.get("window");
+const { width, height } = Dimensions.get("window");
 
 export default function componentName() {
   const router = useRouter();
+  const [collections, setCollections] = useState<CollectionsComponentProps[]>(
+    []
+  );
+
+  useEffect(() => {
+    setCollections([]);
+  }, []);
+
+  if (collections.length === 0) {
+    return (
+      <SafeAreaView
+        style={{
+          flex: 1,
+          width: width,
+          height: height,
+          backgroundColor: appColors.surfaceBright,
+        }}
+      >
+        <Appbar.Header statusBarHeight={0}>
+          <Appbar.BackAction onPress={() => router.back()} />
+          <Appbar.Content title="Collections" />
+        </Appbar.Header>
+        <View
+          style={{
+            width: "100%",
+            height: "100%",
+            alignItems: "center",
+            paddingHorizontal: 50,
+            marginTop: height / 3,
+          }}
+        >
+          <NoElementOnPage
+            title="No Collections Yet!"
+            message="Looks like you haven't made any collections yet—start collecting waste to earn rewards!"
+          />
+        </View>
+      </SafeAreaView>
+    );
+  }
+
   return (
     <SafeAreaView style={styles.container}>
       <Appbar.Header statusBarHeight={0}>
@@ -29,24 +71,9 @@ export default function componentName() {
             gap: 20,
           }}
         >
-          <CollectionsComponent
-            image={require("@/assets/images/user-image.png")}
-            name="John Doe"
-            time="12:00 PM"
-            wasteType="Plastic"
-            size={10}
-            payUnits={100}
-            onPress={() => {}}
-          />
-          <CollectionsComponent
-            image={require("@/assets/images/user-image.png")}
-            name="John Doe"
-            time="12:00 PM"
-            wasteType="Plastic"
-            size={10}
-            payUnits={100}
-            onPress={() => {}}
-          />
+          {collections.map((collection, index) => (
+            <CollectionsComponent key={index} {...collection} />
+          ))}
         </View>
       </ScrollView>
     </SafeAreaView>
